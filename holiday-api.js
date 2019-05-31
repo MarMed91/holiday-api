2581049ff33a7db8f4ace42c609e61d756d8aaba
MarkoM91 <markomedic91@gmail.com>
2019-05-31 13:07:20 +0200

    commit2

--- holiday-api.js
+++ holiday-api.js
@@ -7,9 +7,7 @@ function prevMonth(year,month) {
   printDays(year, month);
   printHolidays(year, month);
 
-
   return month;
-
 }
 
 function nextMonth(year,month) {
@@ -21,15 +19,8 @@ function nextMonth(year,month) {
   printDays(year, month);
   printHolidays(year, month);
 
-
-
   return month;
-
 }
-
-
-
-
 
 function getMonthName(month) {
 
@@ -39,14 +30,11 @@ function getMonthName(month) {
   var monthName = mom.format("MMMM")
 
   return monthName;
-
 }
-
 
 function getMonthDayCount(year, month) {
 
   var mom = moment().year(year).month(month);
-
   var daysCount = mom.daysInMonth();
 
   return daysCount;
@@ -62,7 +50,6 @@ function getHumanDate(year, month, day) {
   var date = mom.format("DD ddd")
 
   return date;
-
 }
 
 function getMachineDate(year, month, day) {
@@ -75,19 +62,15 @@ function getMachineDate(year, month, day) {
   var date = mom.format("YYYY-MM-DD")
 
   return date;
-
 }
-
-
 
 function printTitle(year, month) {
 
   var h1 = $("#month-name");
   var monthName = getMonthName(month);
   var daysCount = getMonthDayCount(year, month);
-  console.log(h1);
-  h1.text(monthName + "  " + year);
 
+  h1.text(monthName + "  " + year);
 }
 
 function printDays(year, month) {
@@ -98,16 +81,14 @@ function printDays(year, month) {
   var template = $("#day-template").html();
   var compiled = Handlebars.compile(template);
 
-
-
     for (var day = 1; day <= daysCount; day++) {
-
 
       var tempDate = {
 
       machineDate : getMachineDate(year, month, day),
       date :getHumanDate(year, month, day)
-      }
+    }
+
       var liDay = compiled(tempDate);
       ulListDay.append(liDay);
     }
@@ -119,6 +100,7 @@ function printHolidays(year, month) {
     year : year,
     month: month
   }
+
   $.ajax({
 
     url: "https://flynn.boolean.careers/exercises/api/holidays",
@@ -135,11 +117,11 @@ function printHolidays(year, month) {
         console.log("Communication error");
       }
     },
-    error: function (request, state, error) {
+   error: function (request, state, error) {
       console.log("request" , date);
       console.log("state" , state);
       console.log("date" , error);
-    }
+   }
   });
 }
 
@@ -152,12 +134,11 @@ function printAll() {
   td.remove();
 }
 
-
 function addHolidayHighlight(holidays) {
 
   var holiday;
-
   for (var i = 0; i < holidays.length; i++) {
+
     holiday = holidays[i];
     var holidayMachine = holiday.date;
     var holidayName = holiday.name;
@@ -167,8 +148,7 @@ function addHolidayHighlight(holidays) {
 
     liHoliday.addClass("red");
     liHoliday.text(liHoliday.text() + " - " + holidayName);
-  }
-;
+  };
 }
 
 function init() {
@@ -183,26 +163,21 @@ function init() {
   var buttonRight = $("input#button-right");
   var buttonLeft = $("input#button-left");
 
-
   buttonRight.click(function() {
+
     if (month < 11) {
 
       month = nextMonth(year, month);
     }
-
-
   });
 
   buttonLeft.click(function() {
+
     if (month > 0) {
 
-    month = prevMonth(year, month);
+      month = prevMonth(year, month);
     }
-
   });
 }
-
-
-
 
 $(document).ready(init);
